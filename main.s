c34b77a59d0b70fe8201acc2c31d1bbf06ba264f
tingkai-h <149487294+tingkai-h@users.noreply.github.com>
2023-11-07 11:51:57 +0000

    chinius-a new dawn

--- main.s
+++ main.s
@@ -9,27 +9,26 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTB
+	movff 	0x06, PORTC
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0x63
 	cpfsgt 	0x06, A
-		call delay
+	
+	
+	movlw 0x10
+	movwf 0x20, A
+	call	delay
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
 delay:
-	movlw 0x00
-
-Dloop:
-	decf 0x11, f, A
-	subwfb 0x10, f, A
-	bc dloop
+	decfsz 0x20, F, A
+	goto $-1
 	return
-   
 	
 	end	main
