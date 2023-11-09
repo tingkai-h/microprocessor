3ffd18a1e7c726be84ad0e596a400775846e983e
tingkai-h <149487294+tingkai-h@users.noreply.github.com>
2023-11-09 14:57:54 +0000

    chinius - light at the end of the tunnel

--- main.s
+++ main.s
@@ -4,39 +4,38 @@ psect	code, abs
 	
 main:
 	org	0x0
+
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
+	movwf	TRISJ, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTC
+	movff 	0x06, PORTJ
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0x63
 	cpfsgt 	0x06, A
-	
-	
-	movlw 0x10
+	movlw 0x03
 	movwf 0x20, A
+	
 	call	delay
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
 delay:
 	decfsz 0x20, F, A
-	call delay2 
-	goto $-1
+	bra delay2
 	return
-	
+
 delay2:
-	movlw 0x10
+	movlw 0x03
 	movwf 0x30, A
 	decfsz 0x30, F, A
 	goto $-1
-	return    
+	bra delay
 	
 	end	main
