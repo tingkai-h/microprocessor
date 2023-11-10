a33aa2b7bf894db815321fb1ff756b3b59542db4
tingkai-h <149487294+tingkai-h@users.noreply.github.com>
2023-11-10 11:46:10 +0000

    pain

--- main.s
+++ main.s
@@ -26,6 +26,25 @@ test:
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
+SPI_MasterInit:	; Set Clock edge to negative
+   bcf	CKE2	; CKE bit in SSP2STAT, 
+   ; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+   movlw 	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK
+   movwf 	SSP2CON1, A
+   ; SDO2 output; SCK2 output
+   bcf	TRISD, PORTD_SDO2_POSN, A	; SDO2 output
+   bcf	TRISD, PORTD_SCK2_POSN, A	; SCK2 output
+   return
+
+SPI_MasterTransmit:  ; Start transmission of data (held in W)
+   movwf 	SSP2BUF, A 	; write data to output buffer
+   
+Wait_Transmit:	; Wait for transmission to complete 
+    btfss 	SSP2IF		; check interrupt flag to see if data has been sent
+    bra 	Wait_Transmit
+    bcf 	SSP2IF		; clear interrupt flag
+    return	
+
 delay:
 	decfsz 0x20, F, A
 	bra delay2
@@ -37,5 +56,6 @@ delay2:
 	decfsz 0x30, F, A
 	goto $-1
 	bra delay
+	
 	
 	end	main
