3db8e2b27f7b40b93ca029c195c8124ae0f44a70
tingkai-h <149487294+tingkai-h@users.noreply.github.com>
2023-11-16 11:58:35 +0000

    le joue est le joue

--- KeyPad.s
+++ KeyPad.s
@@ -9,30 +9,64 @@ KeyPad_output: ds   1
 
 psect	keypad_code,class=CODE
 KeyPad_Setup:
-    bsf	    SPEN	; enable
-    bcf	    SYNC	; synchronous
-    bcf	    BRGH	; slow speed
-    bsf	    TXEN	; enable transmit
-    bcf	    BRG16	; 8-bit generator only
-    movlw   103		; gives 9600 Baud rate (actually 9615)
-    movwf   SPBRG1, A	; set baud rate
-    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin
-					; must set TRISC6 to 1
-    return
+    bsf REPU
+    clrf LATE
+    movlw 0x0F
+    movf TRISE, A
 
-KeyPad_Transmit_Message:	    ; Message stored at FSR2, length stored in W
-    movwf   KeyPad_counter, A
-KeyPad_Loop_message:
-    movf    POSTINC2, W, A
-    call    KeyPad_Transmit_Byte
-    decfsz  KeyPad_counter, A
-    bra	    KeyPad_Loop_message
-    return
+KeyPad_read:
+    
 
-KeyPad_Transmit_Byte:	    ; Transmits byte stored in W
-    btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
-    bra	    KeyPad_Transmit_Byte
-    movwf   TXREG1, A
-    return
+test_none:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_0
+    retlw 0x00
+    
+test_0:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_1
+    retlw 0x00
+    
+test_1:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_2
+    retlw 0x00
 
+test_2:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_3
+    retlw 0x00
 
+test_3:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_4
+    retlw 0x00
+
+test_4:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_5
+    retlw 0x00
+   
+test_5:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_6
+    retlw 0x00
+    
+test_6:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra test_7
+    retlw 0x00
+    
+test_7:
+    movlw 0xFF
+    cpfseq keyval, A
+    bra ;;;;;;;;;;;;;;;
+    retlw 0x00
